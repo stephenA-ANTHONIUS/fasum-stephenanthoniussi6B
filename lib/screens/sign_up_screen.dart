f7c158b7418dafe219157fc984e2fae5f3beab76
stephenA-ANTHONIUS <Stephen.anthonius12345@gmail.com>
2026-04-22 14:03:57 +0700

    Update sign_up_screen.dart

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -78,9 +78,63 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     },
                   ),
                   const SizedBox(height: 16.0,),
-                  TextFormField(),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon (
+                          _isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                                                    
+                        },
+                      ),
+                    ),
+                    obscureText: !_isPasswordVisible,
+                    validator:  (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      if (value.length < 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      return null;
+                    },
+                  ),
                   const SizedBox(height: 16.0,),
-                  TextFormField(),
+                  TextFormField(
+                    controller: _confirmPasswordController,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon (
+                          _isConfirmPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                                                    
+                        },
+                      ),
+                    ),
+                    obscureText: !_isConfirmPasswordVisible,
+                    validator:  (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please confirm your password';
+                      }
+                      if (value != _passwordController.text) {
+                        return 'Passwords do not match';
+                      }
+                      return null;
+                    },
+                  ),
                   const SizedBox(height: 16.0,),
                   TextFormField(),
                   _isLoading ? const CircularProgressIndicator() :
